1903ba1ddc497651104cb6e9c79d674ca6a94705
Liviu Cauia <liviu.cauia@gmail.com>
2020-03-18 11:43:12 +0200

    added new create portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,27 @@
 class PortfoliosController < ApplicationController
   def index
-   @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image)
   end
 end
