aa14edd45d9d2dd24ccd504e115fab0d143638fd
Liviu Cauia <liviu.cauia@gmail.com>
2020-03-23 20:47:54 +0200

    Refactored application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
-
-  before_action :set_session
-
-  def set_session
-    session[:source] = params[:q] if params[:q]
-  end
+  include SetSource
 end
