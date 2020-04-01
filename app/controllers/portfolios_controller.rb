2e3553a08dc1e45e8abede3d58862d53a64d230c
Liviu Cauia <liviu.cauia@gmail.com>
2020-04-01 22:30:33 +0300

    Integrated cocoon for portfolio items nested form component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,7 +21,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio = Portfolio.new
-    3.times { @portfolio.technologies.build }
   end
 
   def create
@@ -75,6 +74,6 @@ class PortfoliosController < ApplicationController
                                       :body,
                                       :thumb_image,
                                       :main_image,
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name, :_destroy])
   end
 end
